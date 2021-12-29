e30353a72e3b36cbe951c93c84b9b044710a11d3
Daniel Graham <dgraham.sprint@gmail.com>
2021-12-28 19:54:07 -0600

    Delete Items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,11 +4,17 @@ class TodoItem extends React.Component {
     render() {
         return (
             <li>
+                
             <input 
             type="checkbox" 
             check={this.props.todo.completed}
             onChange={()=> this.props.handleChangeProps(this.props.todo.id)}
-            /> 
+            />
+
+            <button onclick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+            Delete
+            </button> 
+
             {this.props.todo.title}
             </li> 
             )
